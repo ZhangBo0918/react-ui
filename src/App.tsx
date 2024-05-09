0411342b075d886cb4ebd7a1571e377545f9ead1
aibo chang <aibochang@163.com>
2024-05-09 23:48:00 +0800

    feat:完成水印组件

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,46 @@
-import { IconAdd, IconEmail, Space, Portal } from './ui';
-import './App.css'
-import { ConfigProvider } from './ui/Space/ConfigProvider';
+import { IconAdd, IconEmail, Space, Portal } from "./ui";
+import "./App.css";
+import { ConfigProvider } from "./ui/Space/ConfigProvider";
+import Watermark from "./ui/Watermark";
+import { lyric } from './config';
 function App() {
-
   return (
-    <div className='app'>
+    <div className="app">
       <div>
         <IconAdd />
         <IconEmail spin={true} />
       </div>
       <ConfigProvider space={{ size: 40 }}>
         <Space
-          className='container' 
+          className="container"
           direction="horizontal"
-          align="start" 
+          align="start"
           wrap={true}
           // size={['large', 'small']}
         >
-          <div className='box'>111</div>
-          <div className='box'>222</div>
-          <div className='box'>333</div>
+          <div className="box">111</div>
+          <div className="box">222</div>
+          <div className="box">333</div>
         </Space>
       </ConfigProvider>
-      <div className='portal'>
+      <div className="portal">
         <Portal attach={document.body}>
-          <button  onClick={checkIsPop}>按钮</button>
+          <button onClick={checkIsPop}>按钮</button>
         </Portal>
       </div>
-    </div> 
-  )
+      <div>
+        <Watermark content={["测试水印", "罢演—星弟"]}>
+          <div style={{ height: 800 }}>
+            {lyric.map((item, index) => <p key={index} style={{textAlign:"center"}}>{item}</p>)}
+          </div>
+        </Watermark>
+      </div>
+    </div>
+  );
 }
 
 function checkIsPop(e: any) {
-  console.log('冒泡了', e);
+  console.log("冒泡了", e);
 }
 
-export default App
+export default App;
